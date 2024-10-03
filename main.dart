367d87eca0f05545b4533662baee25e33fde78df
Vaishnavi wavare <140233622+Vaishnaviwavare@users.noreply.github.com>
2024-10-03 08:26:58 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  print("Incubators");
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Core2web",
+      home: TestApp(),
+    );
+  }
+}
+
+class TestApp extends StatelessWidget {
+  const TestApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("First App"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Container(
+            height: 200,
+            width: 200,
+            decoration: const BoxDecoration(
+              color: Colors.amber,
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
